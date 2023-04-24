3edb76ade2bb7ca4c54134e7bc1cadb7efe9f527
fjuton <63885722+fjuton@users.noreply.github.com>
2023-04-24 17:37:59 +0200

    Init

---
+++ src/App.svelte
@@ -0,0 +1,15 @@
+<script>
+
+</script>
+
+<div class="flex w-[100vw] h-[100vh]">
+    <div class="flex flex-col justify-center items-center w-[50%] bg-gray-700">
+        <p class="text-white font-semibold text-2xl">
+            Hi! 👋 I'm @wjuton
+        </p>
+        <p class="text-gray-300 font-semibold text-xl">
+            I like kitties and code stuff from time to time 😊
+        </p>
+    </div>
+    <div></div>
+</div>
